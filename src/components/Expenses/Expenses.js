279d7446e4e1482a396ced43ef5fe32c5de11776
melnard <dejesusmelnard@gmail.com>
2023-04-15 22:36:28 +0800

    filter year version 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,37 +6,27 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const saveFilteredYearHandler = (selectedYear) => {
-    const filteredYear = selectedYear;
-    console.log("in expenses.js");
-    console.log(filteredYear);
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
   };
 
   return (
     <div>
-      
       <Card className="expenses">
-      <ExpensesFilter onSaveFilteredYear={saveFilteredYearHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
